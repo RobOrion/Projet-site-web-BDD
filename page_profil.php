72614c4975d9445e62168e9757e3e14ac8d1e0ba
TIRLITYTY <nguyenterr@eisti.eu>
2017-05-04 10:30:26 +0200

    Update page_profil.php

--- page_profil.php
+++ page_profil.php
@@ -1,1 +1,44 @@
-wookay
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Site </title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="page_profil.css"/>
+	<link rel="icon" href=".jpg"/>
+</head>
+
+<body>
+
+
+<div id = "barre">
+
+ <div id = "titre">
+	<h1> Nom du site </h1>
+ </div>
+
+ <div id = "Profil">
+	<a href="" > Profil </a>
+ </div>
+
+</div>
+
+
+
+<div id="conteneur">
+
+
+</div>
+
+<footer>
+	<img src="../EISTI.jpg" width="10%"/>
+	<center>
+	<p><a href="#"> Chatelet Robin</a>
+	<a href="#"> Hassan Alexandre</a>
+	<a href="#"> Nguyen Terry</a></p>
+</center>
+</footer>
+
+  </body>
+
+</html>
+
