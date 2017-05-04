b841e85a9f1e8cd8f798b8850bc598bed49d9079
TIRLITYTY <nguyenterr@eisti.eu>
2017-05-04 17:53:30 +0200

    Update page_profil.php

--- page_profil.php
+++ page_profil.php
@@ -17,7 +17,7 @@
  </div>
 
  <div id = "Profil">
-	<a href="" > Profil </a>
+	<a href="page_profil.php" id="lien1"> Profil </a>
  </div>
 
 </div>
@@ -25,20 +25,33 @@
 
 
 <div id="conteneur">
+	
+	<div class="bloc"> 
+	<a href="evenement_page.php" class="lien2"> EVENEMENT 
+	</div>
 
+	<div class="bloc">
+	<a href="chat_page.php" class="lien2"> CHAT 
+	</div>
 
+	<div class="bloc">
+	<a href="info_page.php.php" class="lien2"> Informations Personnelles
+	</div>
+
+	<div class="bloc">
+	<a href="accessoire_page.php" class="lien2"> Accessoires 
+	</div>
+
+	<div class="bloc">
+	<a href="aide_page.php" class="lien2"> Aide 
+	</div>
+	
 </div>
 
 <footer>
-	<img src="../EISTI.jpg" width="10%"/>
-	<center>
-	<p><a href="#"> Chatelet Robin</a>
-	<a href="#"> Hassan Alexandre</a>
-	<a href="#"> Nguyen Terry</a></p>
-</center>
+
 </footer>
 
   </body>
 
 </html>
-
