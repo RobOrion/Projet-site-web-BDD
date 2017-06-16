76cf256dcf92545119cd1182ab6e599ab0918473
Merlin2697 <hassanalex@eisti.eu>
2017-06-16 17:28:28 +0200

    Add files via upload

--- chat.php
+++ chat.php
@@ -3,6 +3,14 @@
     <head>
         <meta charset="utf-8" />
         <title>Chat</title>
+        <link rel="stylesheet" href="chat.css"/>
+
+        <!-- BOOTSTRAP CORE STYLE  -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <!-- FONT AWESOME STYLE  -->
+        <!--<link href="assets/css/font-awesome.css" rel="stylesheet" />-->
+        <!-- CUSTOM STYLE  -->
+        <link href="assets/css/style.css" rel="stylesheet" />
     </head>
     <style>
     form {
@@ -11,11 +19,54 @@
     </style>
 
     <body>
-    <form action="chat_post.php" method="post">
+
+      <div class="site" >
+          <div class="container">
+              <div class="site-header">
+                  <button type="button" class="site-icone" data-toggle="collapse" data-target=".site-collapse">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  <a class="left-div" href="index.php">
+
+                      <img src="/img/letter_s.png" width="120%" height="120%"/>
+                  </a>
+
+              </div>
+          </div>
+      </div>
+      <!-- LOGO HEADER END-->
+      <section class="menu-section">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="navbar-collapse collapse ">
+                          <ul id="menu-top" class="nav navbar-nav navbar-right">
+                              <li><a href="index.html" >Accueil</a></li>
+                              <li><a href="page_profil.php">Profils</a></li>
+                              <li><a href="evenement.php">événements</a></li>
+                              <li><a href="covoiturage.php">Covoiturage</a></li>
+                              <li><a href="https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession">Contactez nous</a></li>
+
+                          </ul>
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+      </section>
+
+
+
+
+
+
+    <form id = "form" action="chat_post.php" method="post">
         <p>
-        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
-        <input type="submit" value="Envoyer" />
+
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" />
+        <input  class="btn btn-danger" type="submit" value="Envoyer" />
     </p>
     </form>
 
@@ -42,6 +93,19 @@ while ($donnees = $reponse->fetch())
     echo '<p><strong>' . htmlspecialchars($donnees['login']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
 }
 $reponse->closeCursor();
+
+?>
+
+<?php
+try {
+  $bdd = new PDO('mysql:host=localhost;dbname=profils','root', '');
+}
+catch(Exception $e)
+{
+  die('Erreur : '.$e->getMessage());
+}
+$req = $bdd->prepare("SELECT login FROM profils");
+$req->execute($_POST['login']);
 
 ?>
 
