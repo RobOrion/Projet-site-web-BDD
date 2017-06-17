16e34c218e985bc6aad4da0e69bb702213e2e6e0
Robin Chatelet <robinchatelet21@gmail.com>
2017-06-17 13:38:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <link
+    <link rel="icon" href="letter_s.jpg"/>
 
     <title>SHARE</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
