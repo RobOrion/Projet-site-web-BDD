96499d10db4ef151130a4d85bb606e5ee1fb1243
Merlin2697 <hassanalex@eisti.eu>
2017-06-18 18:38:51 +0200

    Add files via upload

--- page_profil.php
+++ page_profil.php
@@ -5,31 +5,58 @@
 	<meta charset="utf-8"/>
 	<link rel="stylesheet" href="page_profil.css"/>
 	<link rel="icon" href=".jpg"/>
+
+	<link rel="stylesheet" href="chat.css"/>
+
+	<!-- BOOTSTRAP CORE STYLE  -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet" />
+	<!-- FONT AWESOME STYLE  -->
+	<!--<link href="assets/css/font-awesome.css" rel="stylesheet" />-->
+	<!-- CUSTOM STYLE  -->
+	<link href="assets/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
-	<?php 
+	<?php
 		include('fonction.php');
 		logged_only();
 	?>
-	<div id = "barre">
-	<div id = "titre">
-		<a href="site.php"> <img src="/img/letter_s.png" href="site.php" width="120%"/> </a>
-	</div>
-	
-		<div class="right-div">
-                <a href="deconnexion.php" class="btn btn-danger pull-right">Déconnexion</a>
-            </div>
+	<div class="site" >
+			<div class="container">
+					<div class="site-header">
+							<button type="button" class="site-icone" data-toggle="collapse" data-target=".site-collapse">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+							</button>
+							<a class="left-div" href="index.php">
 
-	<div id = "barre2">
-		<div id = "cherch">
-			<i>Chercher un utilisateur :</i>
-			<input type = "text" name = "utilisateur"/>
-			<i>Chercher un évenement :</i>
-			<input type = "text" name = "évenement"/>
-			<input id = "valider" type="submit" value="Rechercher"/>
-		</div>
+									<img src="img/letter_s.png" width="120%" height="120%"/>
+							</a>
+
+					</div>
+			</div>
 	</div>
+	<!-- LOGO HEADER END-->
+	<section class="menu-section">
+			<div class="container">
+					<div class="row">
+							<div class="col-md-12">
+									<div class="navbar-collapse collapse ">
+											<ul id="menu-top" class="nav navbar-nav navbar-right">
+													<li><a href="index.html" >Accueil</a></li>
+													<li><a href="page_profil.php">Profils</a></li>
+													<li><a href="evenement.php">événements</a></li>
+													<li><a href="covoiturage.php">Covoiturage</a></li>
+													<li><a href="https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession">Contactez nous</a></li>
+
+											</ul>
+									</div>
+							</div>
+
+					</div>
+			</div>
+	</section>
 
 
 	<div id = "Profil">
@@ -38,7 +65,7 @@
 	</div>
 
 	<div id="conteneur">
-	
+
 	<div class="bloc">
 		<a href="evenement_page.php" class="lien2"> EVENEMENT
 	</div>
